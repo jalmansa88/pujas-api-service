83f6b128916018391fc7e1ddc2e39294fb884d9b
lordneru <lordneru@gmail.com>
2018-02-14 22:36:16 +0100

    adding new main methods

--- main.js
+++ main.js
@@ -1,11 +1,34 @@
 var express = require("express");
+var jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+var mongoAccess = require("mongodb").MongoClient;
+
 var app = express();
 
-var jwt = require('jsonwebtoken');
+function Usuario(username, pass, name){
+    this.username = username;
+    this.pass = pass;
+    this.name = name;
+}
 
-const crypto = require('crypto');
+function Puja(username, amount, event){
+    this.username = username;
+    this.amount = amount;
+    this.evento = evento;
+}
 
-var mongoAccess = require("mongodb").MongoClient;
+function Evento(name, description){
+    this.name = name;
+    this.description = description;
+}
+
+app.post("/user/create", function(req, res){
+    //create new user
+});
+
+app.get("/", function(req, res){
+   res.send("GET Works!");
+});
 
 app.listen(8080,
 function(){
